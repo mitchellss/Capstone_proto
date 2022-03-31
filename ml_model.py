ed70a2962d3d1310114e22d0443fe36709871068
mitchellss <stephen.mitchell2299@gmail.com>
2022-03-31 02:03:08 +0000

    new models

--- ml_model.py
+++ ml_model.py
@@ -50,7 +50,7 @@ for sensor_id in sensor_titles:
         
         features = FEATURES_3 + [f"{sensor_id}_original"]
         
-        df[features].to_csv("test.csv", index=False)
+        # df[features].to_csv("test.csv", index=False)
         # sys.exit(0)
                 
         X = df[features].values
@@ -66,35 +66,31 @@ for sensor_id in sensor_titles:
 
         # np.save(PATH / args.path / "ml" / "data" / f"{sensor_id}_x", X)
         # np.save(PATH / args.path / "ml" / "data" / f"{sensor_id}_y", y)
-        for epoch in range(10,200,5):
-            for i in range(0,20):
             
-                model = Sequential()
-                model.add(Dense(500, input_dim=len(features), activation="relu"))
-                model.add(Dense(100, activation= "relu"))
-                model.add(Dense(50, activation= "relu"))
-                model.add(Dense(1))
-                
-                model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
-                model.fit(X_train, y_train, epochs=epoch)
-                
-                
-                # pred_train = model.predict(X_train)
-                # train_err = (np.sqrt(mean_squared_error(y_train,pred_train)))
+        model = Sequential()
+        model.add(Dense(500, input_dim=len(features), activation="relu"))
+        model.add(Dense(100, activation= "relu"))
+        model.add(Dense(50, activation= "relu"))
+        model.add(Dense(1))
+        
+        model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
+        model.fit(X_train, y_train, epochs=EPOCHS, verbose=0)
+        
+        
+        # pred_train = model.predict(X_train)
+        # train_err = (np.sqrt(mean_squared_error(y_train,pred_train)))
 
-                # pred = model.predict(X_test)
-                # test_err = (np.sqrt(mean_squared_error(y_test,pred)))
+        # pred = model.predict(X_test)
+        # test_err = (np.sqrt(mean_squared_error(y_test,pred)))
+        
+        pred_new = model.predict(X2)
+        new_err = (np.sqrt(mean_squared_error(y2,pred_new)))
+        
+        # with open("results_3.csv", "a") as file:
+        #     file.write(f"{epoch},{new_err}\n")
+        print(f"{new_err}")
+        
                 
-                pred_new = model.predict(X2)
-                new_err = (np.sqrt(mean_squared_error(y2,pred_new)))
-                
-                with open("results_3.csv", "a") as file:
-                    file.write(f"{epoch},{new_err}\n")
-                # print(f"{new_err}")
-        
-        
-        sys.exit(0)
-        
         if args.save:
             model.save(PATH / args.path / "ml" / "models" / f"{sensor_id}_newmodel_2.keras")
 
